4432e751302f52947252deb73d5007ee67e05105
Stas-Ukrop <St.kr342@gmail.com>
2023-03-13 10:52:01 +0100

    add redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,29 @@
-import Nav from '../Nav/Nav'
-import { Header } from 'components/Header/Header'
+import styles from './App.module.css';
+import { Header } from 'components/Header/Header';
 
 function App() {
-  const mass = ["О нас", "Оплата", "Контракты", "Отзывы", "Акция"]
-  const navList = ['Бакалея', 'Сухофрукты', 'Овощи', 'Варенья и соленья', 'Сладости', 'Орехи', 'Грибы', 'Зелень', 'Ягоды и фрукты', 'Напитки', 'Молочные продукты', 'Морепродукты', 'Мясо']
-  console.log(mass)
-  return (<>
-    <Header list={mass} navList={navList}></Header>
-  </>
-  )
+  const mass = ['О нас', 'Оплата', 'Контракты', 'Отзывы', 'Акция'];
+  const navList = [
+    'Бакалея',
+    'Сухофрукты',
+    'Овощи',
+    'Варенья и соленья',
+    'Сладости',
+    'Орехи',
+    'Грибы',
+    'Зелень',
+    'Ягоды и фрукты',
+    'Напитки',
+    'Молочные продукты',
+    'Морепродукты',
+    'Мясо',
+  ];
+  console.log(mass);
+  return (
+    <div className={styles.containerHeader}>
+      <Header list={mass} navList={navList}></Header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import 'modern-normalize/modern-normalize.css';
+import './styles/base.css';
 import App from './components/App/App';
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+);
