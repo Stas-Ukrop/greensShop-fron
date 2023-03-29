35d7c81a41b0b44da0c70cbc29bc46b48e497913
Stas-Ukrop <St.kr342@gmail.com>
2023-03-29 23:32:45 +0200

    fix navBar

--- src/components/NavList/NavList.js
+++ src/components/NavList/NavList.js
@@ -11,73 +11,73 @@ export const NavList = () => {
         </NavLink>
       </li>
       <li>
-        <NavLink to="/driedFruits">
+        <NavLink to="/driedFruits" className={styles.styleLink}>
           <img src={svg[1]} alt="driedFruits" />
           <span className={styles.navListSpan}>Сухофрукты</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/vegetables">
+        <NavLink to="/vegetables" className={styles.styleLink}>
           <img src={svg[2]} alt="vegetables" />
           <span className={styles.navListSpan}>Овощи</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/jamAndPickles">
+        <NavLink to="/jamAndPickles" className={styles.styleLink}>
           <img src={svg[3]} alt="jamAndPickles" />
           <span className={styles.navListSpan}>Варенья и соленья</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/sweets">
+        <NavLink to="/sweets" className={styles.styleLink}>
           <img src={svg[4]} alt="sweets" />
           <span className={styles.navListSpan}>Сладости</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/nuts">
+        <NavLink to="/nuts" className={styles.styleLink}>
           <img src={svg[5]} alt="nuts" />
           <span className={styles.navListSpan}>Орехи</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/mooshrooms">
+        <NavLink to="/mooshrooms" className={styles.styleLink}>
           <img src={svg[6]} alt="mooshrooms" />
           <span className={styles.navListSpan}>Грибы</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/greens">
+        <NavLink to="/greens" className={styles.styleLink}>
           <img src={svg[7]} alt="greens" />
           <span className={styles.navListSpan}>Зелень</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/berryAndFruits">
+        <NavLink to="/berryAndFruits" className={styles.styleLink}>
           <img src={svg[8]} alt="berryAndFruits" />
           <span className={styles.navListSpan}>Ягоды и фрукты</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/drinks">
+        <NavLink to="/drinks" className={styles.styleLink}>
           <img src={svg[9]} alt="drinks" />
           <span className={styles.navListSpan}>Напитки</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/milks">
+        <NavLink to="/milks" className={styles.styleLink}>
           <img src={svg[10]} alt="milks" />
           <span className={styles.navListSpan}>Молочные продукты</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/fish">
+        <NavLink to="/fish" className={styles.styleLink}>
           <img src={svg[11]} alt="fish" />
           <span className={styles.navListSpan}>Морепродукты</span>
         </NavLink>
       </li>
       <li>
-        <NavLink to="/meat">
+        <NavLink to="/meat" className={styles.styleLink}>
           {' '}
           <img src={svg[12]} alt="meat" />
           <span className={styles.navListSpan}>Мясо</span>
